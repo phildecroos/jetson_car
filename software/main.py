7748bf5cc421f22d4cdf1359426900c6bdb66cd1
phildecroos <decroosphil@gmail.com>
2023-10-07 18:17:05 -0700

    Add code structure

---
+++ software/main.py
@@ -0,0 +1,11 @@
+# main driving program
+
+# imports
+
+# def functions & setup code
+
+# try:
+#   start sensors, lights, & cv threads
+#   wait for start button press then start motor control thread
+# finally:
+#   shutdown code to stop motors & make all outputs low
